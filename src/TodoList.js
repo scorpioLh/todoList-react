9d00ce335b19225e66987eff8356f2302cd6ea26
linhan <scorpio360@163.com>
2019-01-06 10:56:08 +0800

    ui组件和功能组件拆分，并将ui组件改为无状态组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd'
 import store from './store'
 import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators'
+import TodoListUI from './TodoListUI'
 
 class TodoList extends Component {
 	constructor(props) {
@@ -11,38 +11,29 @@ class TodoList extends Component {
 		this.handleInputChange = this.handleInputChange.bind(this)
 		this.handleStoreChange = this.handleStoreChange.bind(this)
 		this.handleBtnClick = this.handleBtnClick.bind(this)
+		this.handleItemClick = this.handleItemClick.bind(this)
 		store.subscribe(this.handleStoreChange) // 只要store发生改变，就会执行参数内的方法
 	}
 
 	render() {
 		return (
-			<div style={{marginTop: '10px', marginLeft: '10px'}}>
-				<div>
-					<Input
-						value={this.state.inputValue}
-						placeholder='todo info'
-						style={{width: '300px', marginRight: '10px'}}
-						onChange={this.handleInputChange}
-					/>
-					<Button type='primary' onClick={this.handleBtnClick}>提交</Button>
-				</div>
-				<List
-					style={{marginTop: '10px', width: '300px'}}
-					bordered
-					dataSource={this.state.list}
-					renderItem={(item, index) => (<List.Item onClick={this.handleItemClick.bind(this, index)}>{item}</List.Item>)}
-				/>
-			</div>
+			<TodoListUI
+				inputValue={this.state.inputValue}
+				handleInputChange={this.handleInputChange}
+				handleBtnClick={this.handleBtnClick}
+				list={this.state.list}
+				handleItemClick={this.handleItemClick}
+			/>
 		)
+	}
+
+	handleStoreChange() {
+		this.setState(store.getState())
 	}
 
 	handleInputChange(e) {
 		const action = getInputChangeAction(e.target.value)
 		store.dispatch(action) // 提交事件给store,store会自动将他传给reducer
-	}
-
-	handleStoreChange() {
-		this.setState(store.getState())
 	}
 
 	handleBtnClick() {
